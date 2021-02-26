551e4f623cce7e96d24053986e556b4fa04d9894
dchally <dchally@posportal.com>
2021-02-26 13:46:06 -0800

    Add start files

---
+++ src/main/java/com/chally/build/TestUtil.java
@@ -0,0 +1,15 @@
+package com.chally.build;
+
+/**
+ * Just a simple utility class to do something.
+ * 
+ * @author dchally
+ *
+ */
+public class TestUtil {
+	private TestUtil() {}
+	
+	public static String getText() {
+		return "1";
+	}
+}
