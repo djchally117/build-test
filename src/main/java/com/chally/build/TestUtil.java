6dee0aa17a733b5a50aec08314ac22dffee6554e
dchally <dchally@posportal.com>
2021-03-02 09:43:29 -0800

    Add code for unit testing

--- src/main/java/com/chally/build/TestUtil.java
+++ src/main/java/com/chally/build/TestUtil.java
@@ -1,5 +1,7 @@
 package com.chally.build;
 
+import java.util.Date;
+
 /**
  * Just a simple utility class to do something.
  * 
@@ -9,7 +11,43 @@ package com.chally.build;
 public class TestUtil {
 	private TestUtil() {}
 	
-	public static String getText() {
-		return "5";
+	public static String getString() {
+		return "Hellow World";
+	}
+	
+	public static Character getCharacter() {
+		return 'A';
+	}
+	
+	public static Byte getByte() {
+		return 123;
+	}
+	
+	public static Short getShort() {
+		return 1234;
+	}
+	
+	public static Integer getInteger() {
+		return 123456;
+	}
+	
+	public static Long getLong() {
+		return 123456789L;
+	}
+	
+	public static Float getFloat() {
+		return 23.45f;
+	}
+	
+	public static Double getDouble() {
+		return 123.456;
+	}
+	
+	public static Boolean getBoolean() {
+		return true;
+	}
+	
+	public static Date getDate() {
+		return new Date();
 	}
 }
