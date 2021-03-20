3f157d9676480511cc0446f83d4b556d9ac5a4a7
Daniel Chally <djchally117@yahoo.com>
2021-03-19 17:56:31 -0700

    Resource filtering for version number

--- src/main/java/com/chally/build/Util.java
+++ src/main/java/com/chally/build/Util.java
@@ -1,6 +1,7 @@
 package com.chally.build;
 
 import java.util.Date;
+import java.util.ResourceBundle;
 
 /**
  * Just a simple utility class to do something.
@@ -9,45 +10,52 @@ import java.util.Date;
  *
  */
 public class Util {
-	private Util() {}
-	
+	private Util() {
+	}
+
 	public static String getString() {
 		return "Hello World";
 	}
-	
+
 	public static Character getCharacter() {
 		return 'G';
 	}
-	
+
 	public static Byte getByte() {
 		return 123;
 	}
-	
+
 	public static Short getShort() {
 		return 1234;
 	}
-	
+
 	public static Integer getInteger() {
 		return 123456;
 	}
-	
+
 	public static Long getLong() {
 		return 123456789L;
 	}
-	
+
 	public static Float getFloat() {
 		return 23.45f;
 	}
-	
+
 	public static Double getDouble() {
 		return 123.456;
 	}
-	
+
 	public static Boolean getBoolean() {
 		return true;
 	}
-	
+
 	public static Date getDate() {
 		return new Date();
+	}
+
+	public static void main(String[] args) {
+		ResourceBundle resource = ResourceBundle.getBundle("test");
+		String version = resource.getString("version");
+		System.out.println(version);
 	}
 }
